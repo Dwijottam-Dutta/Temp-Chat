dff7e7f5af8d2438273b9882b3b571077dde5f8b
Dwijottam Dutta <dwijottamdutta@gmail.com>
2023-12-27 11:58:57 +0530

    fix

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from string import ascii_uppercase
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "hjhjsdahhds"
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 rooms = {}
 
@@ -101,4 +101,3 @@ def disconnect():
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
-    allow_unsafe_werkzeug=True
